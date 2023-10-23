4cb64d604fe4e135057f42ce3aacdb00dc10ac03
Adam Cozzette <acozzette@google.com>
2023-10-23 23:02:10 +0000

    Exclude upb generated code from include guards check

--- tools/distrib/check_include_guards.py
+++ tools/distrib/check_include_guards.py
@@ -256,6 +256,8 @@ for filename in filename_list:
         or filename.endswith(".upb.c")
         or filename.endswith(".upbdefs.h")
         or filename.endswith(".upbdefs.c")
+        or filename.endswith(".upb_minitable.h")
+        or filename.endswith(".upb_minitable.c")
     ):
         continue
     ok = ok and validator.check(filename, args.fix)
